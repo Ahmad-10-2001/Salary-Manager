6ba7bb6aea0a7585ebdd1f544c00533a3d6be7c1
Ahmad-10-2001 <153617624+Ahmad-10-2001@users.noreply.github.com>
2026-09-12 15:53:39 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "salary-manager-v2";
+const CACHE_NAME = "salary-manager-v3";
 
 const FILES_TO_CACHE = [
   "./",
@@ -8,64 +8,34 @@ const FILES_TO_CACHE = [
   "./manifest.json"
 ];
 
-
-// Install service worker and save app files
 self.addEventListener("install", (event) => {
-
   event.waitUntil(
-
     caches.open(CACHE_NAME)
       .then((cache) => {
-
         return cache.addAll(FILES_TO_CACHE);
-
       })
-
   );
-
 });
 
-
-// Activate and remove old cache versions
 self.addEventListener("activate", (event) => {
-
   event.waitUntil(
-
     caches.keys().then((cacheNames) => {
-
       return Promise.all(
-
         cacheNames.map((cacheName) => {
-
           if(cacheName !== CACHE_NAME){
-
             return caches.delete(cacheName);
-
           }
-
         })
-
       );
-
     })
-
   );
-
 });
 
-
-// Load from cache when offline
 self.addEventListener("fetch", (event) => {
-
   event.respondWith(
-
     caches.match(event.request)
       .then((response) => {
-
         return response || fetch(event.request);
-
       })
-
   );
-
 });
